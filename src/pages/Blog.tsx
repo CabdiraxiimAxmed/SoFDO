18dbbb899b3e2dbdc7a0d046cc9b3a0fc9255385
Abdirahim Ahmed <abdirahimiahmed@gmail.com>
2023-09-01 09:06:24 +0300

    fixed blog view

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,13 +1,18 @@
 import sofdo_report from "../assets/images/sofdo-report.jpeg"
+import { useNavigate } from 'react-router-dom'
 
 const Blog: React.FC = () => {
+  const navigate = useNavigate();
+  const blogView = (page: string) => {
+    navigate(`/blog/${page}`)
+  }
   return <div className='blog-card-container'>
     <div className='blog-card'>
       <img src={sofdo_report}/>
       <div style={{width: "100%", padding: "10px 0px"}}>
         <h3> Annual Report 2023 </h3>
       </div>
-      <a className='read-more' href='/blog/annual-report-2023'> Read More </a>
+      <button className='read-more' onClick={() => blogView('annual-report-2023')}> Read More </button>
     </div>
   </div>
 };
