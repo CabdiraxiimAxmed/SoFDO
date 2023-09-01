298b3076b709d9d4ef1c9bdf5cb1843be4341b0b
Abdirahim Ahmed <abdirahimiahmed@gmail.com>
2023-09-01 09:12:36 +0300

    fixed preview

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -1,11 +1,13 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
+import Footer from '../components/Footer.tsx';
 interface Props {
   text: string;
 }
 const Preview: React.FC<Props> = ({ text }) => {
   return <div className='preview-container'>
     <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />
+    <Footer/>
   </div>
 
 }
